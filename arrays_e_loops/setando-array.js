ff55041353e8ab8459fca75eac51ed5bc8737eda
Shintanuies <gomesvidalbrunorosa@gmail.com>
2025-06-12 21:15:20 -0300

    add arc js

---
+++ arrays_e_loops/setando-array.js
@@ -0,0 +1,12 @@
+const nomesDeEstudantes = ['jose', 'joao', 'maria', 'joana', 'andre'];
+
+console.log(nomesDeEstudantes);
+
+const x = 3;
+
+nomesDeEstudantes[0] = 'andreia';
+nomesDeEstudantes[1] = 'andreia';
+nomesDeEstudantes[2] = 'andreia';
+nomesDeEstudantes[x] = 'andreia';
+
+console.log(nomesDeEstudantes);
